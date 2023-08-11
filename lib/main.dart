cc61bf23255795f1d1c53115640da56499f16154
idil altun <idilaltun12@gmail.com>
2023-08-11 14:35:09 +0300

    login page added

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Example',
+      title: 'Login ExampleXXS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
